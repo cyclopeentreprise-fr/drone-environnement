5d439b5c91ad03833c36812bbcc651cc7972bbef
cyclopeentreprise-fr <cyclope.entreprise@gmail.com>
2025-10-22 10:13:57 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  experimental: {
+    typedRoutes: true,
+  },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
