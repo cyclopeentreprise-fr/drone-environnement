e86b8f7f5b2586fd9fe7baa693cd94c78cf19608
cyclopeentreprise-fr <cyclope.entreprise@gmail.com>
2025-10-22 14:21:46 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Next 15
   typedRoutes: true,
+  // Skip ESLint during Vercel builds (tu pourras le réactiver plus tard)
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
 export default nextConfig;
