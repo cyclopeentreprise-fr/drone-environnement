102975ce6c3c88cc038a92327e3ab37989ef922e
cyclopeentreprise-fr <cyclope.entreprise@gmail.com>
2025-10-22 11:36:31 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body className="bg-black text-slate-100 antialiased">{children}</body>
+      <body className="bg-black text-slate-100 antialiased">
+        {children}
+      </body>
     </html>
   );
 }
