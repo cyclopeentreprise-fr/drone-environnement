8c2757903677916fd4a82b2f263da3176379b8b7
cyclopeentreprise-fr <cyclope.entreprise@gmail.com>
2025-10-22 11:20:42 +0200

    Create card.tsx

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,17 @@
+```tsx
+import * as React from "react";
+import clsx from "clsx";
+
+
+export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+return <div className={clsx("rounded-2xl border bg-white/5", className)} {...props} />;
+}
+
+
+export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+return <div className={clsx("p-6", className)} {...props} />;
+}
+```
+
+
+---
