b4c32fd4daba15d6691646c2eb242826bc608c01
cyclopeentreprise-fr <cyclope.entreprise@gmail.com>
2025-10-22 10:13:00 +0200

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/pages/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
