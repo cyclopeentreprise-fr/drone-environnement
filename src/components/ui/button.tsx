4b09f81b47c1f70a101e2df88ce254b8d21285a1
cyclopeentreprise-fr <cyclope.entreprise@gmail.com>
2025-10-22 11:36:52 +0200

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,24 +1,20 @@
-```tsx
 "use client";
 import * as React from "react";
 import clsx from "clsx";
 
-
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & { asChild?: boolean };
 
-
 export function Button({ className, asChild, ...props }: Props) {
-const Cmp: any = asChild ? "span" : "button";
-return (
-<Cmp
-className={clsx(
-"inline-flex items-center justify-center rounded-xl px-4 py-2 text-sm font-medium",
-"bg-black text-white hover:bg-black/90",
-"disabled:opacity-50 disabled:pointer-events-none",
-className
-)}
-{...props}
-/>
-);
+  const Cmp: any = asChild ? "span" : "button";
+  return (
+    <Cmp
+      className={clsx(
+        "inline-flex items-center justify-center rounded-xl px-4 py-2 text-sm font-medium",
+        "bg-black text-white hover:bg-black/90",
+        "disabled:opacity-50 disabled:pointer-events-none",
+        className
+      )}
+      {...props}
+    />
+  );
 }
-```
